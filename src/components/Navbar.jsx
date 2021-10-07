da2470e0da3284f90db91d62e41db90bb0705c8f
anarysumbetov <74860324+anarysumbetov@users.noreply.github.com>
2021-10-07 17:26:37 +0600

    Links created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { 
-    // Button, Menu, 
-    Typography, Avatar } from 'antd';
+    // Button, 
+    Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
-import { 
-    // HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined
-} from '@ant-design/icons';
+import { FundOutlined, HomeOutlined, MoneyCollectOutlined, BulbOutlined, MenuOutlined } from '@ant-design/icons';
 
 import icon from '../images/cryptocurrency.png'
+
 const Navbar = () => {
     return (
         <div className="nav-container">
@@ -16,10 +15,21 @@ const Navbar = () => {
                 <Typography.Title level={2} className="logo">
                     <Link to="/">Cryptoverse</Link>
                 </Typography.Title>
-                {/* <Button className="menu-control-container">
-                
-                </Button> */}
             </div>
+            <Menu theme="dark">
+                <Menu.Item icon={<HomeOutlined />}>
+                    <Link to="/">Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined />}>
+                    <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined />}>
+                    <Link to="/exchanges">Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined />}>
+                    <Link to="/news">News</Link>
+                </Menu.Item>
+            </Menu>
         </div>
     )
 }
