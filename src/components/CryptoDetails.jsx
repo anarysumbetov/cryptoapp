50ea1ea4124e8208d6de3b30764a229c735e339f
anarysumbetov <74860324+anarysumbetov@users.noreply.github.com>
2021-12-04 21:30:21 +0600

    CryptoDetails was fetching

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -15,10 +15,80 @@ const CryptoDetails = () => {
     const [timePeriod, setTimePeriod] = useState("7d");
     const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
 
+    const cryptoDetails = data?.data?.coin;
+
+    console.log("cryptoDetails", cryptoDetails);
+    const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
+
+    let stats;
+    if (cryptoDetails) {
+    stats = [
+            { title: 'Price to USD', value: `$ ${cryptoDetails.price && millify(cryptoDetails.price)}`, icon: <DollarCircleOutlined /> },
+            { title: 'Rank', value: cryptoDetails.rank, icon: <NumberOutlined /> },
+            { title: '24h Volume', value: `$ ${cryptoDetails.volume && millify(cryptoDetails.volume)}`, icon: <ThunderboltOutlined /> },
+            { title: 'Market Cap', value: `$ ${cryptoDetails.marketCap && millify(cryptoDetails.marketCap)}`, icon: <DollarCircleOutlined /> },
+            { title: 'All-time-high(daily avg.)', value: `$ ${millify(cryptoDetails.allTimeHigh.price)}`, icon: <TrophyOutlined /> },
+        ]
+    }
+
+
+
+    let genericStats 
+    if (cryptoDetails) {
+    genericStats= [
+        { title: 'Number Of Markets', value: cryptoDetails.numberOfMarkets, icon: <FundOutlined /> },
+        { title: 'Number Of Exchanges', value: cryptoDetails.numberOfExchanges, icon: <MoneyCollectOutlined /> },
+        { title: 'Aprroved Supply', value: cryptoDetails.approvedSupply ? <CheckOutlined /> : <StopOutlined />, icon: <ExclamationCircleOutlined /> },
+        { title: 'Total Supply', value: `$ ${millify(cryptoDetails.totalSupply)}`, icon: <ExclamationCircleOutlined /> },
+        { title: 'Circulating Supply', value: `$ ${millify(cryptoDetails.circulatingSupply)}`, icon: <ExclamationCircleOutlined /> },
+    ]
+    }
+    
+    console.log("crypto details ", cryptoDetails);
     return (
-        <div>
-            CryptoDetails {coinId}
-        </div>
+       <>
+            <Col className="coin-detail-container">
+                <Col className="coin-heading-container">
+                    <Title level={2} className="coin-name">
+                        {cryptoDetails && cryptoDetails.name} ({cryptoDetails && cryptoDetails.slug}) Price
+                    </Title>
+
+                    <p>
+                        {cryptoDetails && cryptoDetails.name} live price in US dollars.
+                        View value statistics, market cap and supply.
+                    </p>
+                </Col>
+                <Select
+                    defaultValue="7d" 
+                    className="select-timerperiod" 
+                    placeholder="Select Time Period" 
+                    onChange={(value) => setTimePeriod(value)}                  
+                >
+                    {time.map((item) => <Option key={item}>{item}</Option>)}
+                </Select>
+                <Col className="stats-container">
+                    <Col className="coin-value-statistics">
+                        <Col className="coin-value-statistics-heading">
+                            <Title level={3} className="coin-details-heading">
+                                {cryptoDetails && cryptoDetails.name} Value Statistics
+                            </Title>
+                            <p>
+                            An overview showing the stats of {cryptoDetails && cryptoDetails.name}
+                            </p>
+                        </Col>
+                        {cryptoDetails && stats && stats.map(({ icon, title, value}) => (
+                        <Col className="coin-stats">
+                            <Col className="coin-stats-name">
+                                <Text>{icon}</Text>
+                                <Text>{title}</Text>
+                                <Text>{value}</Text>
+                            </Col>                                   
+                        </Col>
+                        ))}
+                    </Col>
+                </Col>
+            </Col>
+       </>
     )
 }
 
